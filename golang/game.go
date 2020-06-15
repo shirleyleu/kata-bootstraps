cc8e69784beca63df999fdf84f3acf9691eb5ad0
Shirley Leu <shirley.leu@adevinta.com>
2020-06-15 14:59:45 +0200

    refactor frames array initialization

--- golang/game.go
+++ golang/game.go
@@ -3,35 +3,35 @@ package main
 import "errors"
 
 type game struct {
-	frames     []frame
+	frames     []frame // 10 frames in each game
 	scoreCount int
 	spare      bool
 }
 
 type frame struct {
-	rolls []int
+	rolls []int // [1, 2] (max de 3)
 }
 
-func (g *game) roll(n int) error {
-	if n > 10 {
+func (g *game) roll(pinsKnockedDown int) error {
+	if pinsKnockedDown > 10 {
 		return errors.New("cannot knock down more than 10 pins")
 	}
 
 	if g.frames == nil {
-		g.frames = make([]frame, 1)
+		g.frames = make([]frame, 1, 10)
 	}
 
 	lastFrameIndex := len(g.frames) - 1
 
 	if g.spare {
-		g.scoreCount += n
+		g.scoreCount += pinsKnockedDown
 		g.spare = false
 	}
 
 	if len(g.frames[lastFrameIndex].rolls) == 2 {
-		g.frames = append(g.frames, frame{rolls: []int{n}})
+		g.frames = append(g.frames, frame{rolls: []int{pinsKnockedDown}})
 	} else {
-		g.frames[lastFrameIndex].rolls = append(g.frames[lastFrameIndex].rolls, n)
+		g.frames[lastFrameIndex].rolls = append(g.frames[lastFrameIndex].rolls, pinsKnockedDown)
 		var totalScore int
 		for _, v := range g.frames[lastFrameIndex].rolls {
 			totalScore += v
@@ -41,7 +41,7 @@ func (g *game) roll(n int) error {
 		}
 	}
 
-	g.scoreCount += n
+	g.scoreCount += pinsKnockedDown
 
 	return nil
 }
